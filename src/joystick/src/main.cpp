22fb5bf352223f7c26140cc0c9f57cd943cc2d38
hinpak <whpak@connect.ust.hk>
2025-01-17 09:35:21 +0800

    debug

--- src/joystick/src/main.cpp
+++ src/joystick/src/main.cpp
@@ -10,8 +10,12 @@
 
 using namespace std;
 
+#define AXIS_LR 0 // left right
+#define AXIS_UD 1 // up down
+
 const std::string JOYSTICK_DEV = "/dev/input/js0";
 int joy_prev[2] = {0, 0}; // left, right
+float volt_prev[2] = {0.0f, 0.0f};
 
 class JoystickNode : public rclcpp::Node
 {
@@ -46,38 +50,85 @@ private:
       if (event.isButton())
       {
         // type = JS_EVENT_BUTTON;
-        std::cout << "Button " << static_cast<int>(event.number) << " is " << (event.value == 0 ? "up" : "down") << std::endl;
-        if (number == 13 && event.value == 1)
+        // using Button 3 as reset button (square button)
+        if (number == 3 && event.value == 1)
         {
+          std::cout << "Button " << static_cast<int>(event.number) << " is " << (event.value == 0 ? "up" : "down") << std::endl;
           // reset button
           joy[0] = 0; joy[1] = 0;
           std::cout << "Reset Joystick" << std::endl;
+
+          // publish the message
+          canbus_slave::msg::FocAngle volt_msg;
+          volt_msg.left = 0.0f;
+          volt_msg.right = 0.0f;
+          volt_pub_->publish(volt_msg);
+          std::cout << "Pub Voltage Left: " << 0.0f << " Right: " << 0.0f << std::endl;
+          
+          // update the previous value
+          joy_prev[0] = joy[0];
+          joy_prev[1] = joy[1];
+          for (int i = 0; i < 2; i++)
+          {
+            // joy_prev[i] = joy[i];
+            volt_prev[i] = 0.0f;
+          }
+
+          std::cout << "-----------------" << std::endl;
         }
         else return;
       }
       else if (event.isAxis())
       {
         // type = JS_EVENT_AXIS;
-        std::cout << "Axis " << static_cast<int>(event.number) << " is at position " << event.value << std::endl;
-        if (number == 0 || number == 1)
+        // using the left joystick
+        if (number == AXIS_LR || number == AXIS_UD)
         {
-          // update value
-          // std::cout << "prev: " << joy_prev[0] << " " << joy_prev[1] << std::endl;
-          joy[number] = event.value;
-          // std::cout << "current: " << joy[0] << " " << joy[1] << std::endl;
+          std::cout << "Axis " << static_cast<int>(event.number) << " is at position " << event.value << std::endl;
+
+          // filter
+          if ((abs(event.value) < 260) || (abs(event.value) < 2200 && joy_prev[number] == 0))
+          {
+            joy[number] = 0;
+          }
+          // else if (abs(event.value) < 2200 && joy_prev[number] == 0)
+          // {
+          //   joy[number] = 0;
+          // }
+          else
+          {
+            joy[number] = event.value;
+          }
 
           float volt[2] = {0.0f, 0.0f};
           joy2theworld(joy[0], joy[1], volt[0], volt[1]);
+          for (int i = 0; i < 2; i++)
+          {
+            if (abs(volt[i]) < 2.0f)
+            {
+              volt[i] = 0;
+            }
+          }
 
           // publish the reading as voltage
-          canbus_slave::msg::FocAngle volt_msg;
-          volt_msg.left = volt[0];
-          volt_msg.right = volt[1];
-          std::cout << "Pub Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
-          volt_pub_->publish(volt_msg);
+          if (volt[number] != volt_prev[number])
+          {
+            // std::cout << "Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
+            canbus_slave::msg::FocAngle volt_msg;
+            volt_msg.left = volt[0];
+            volt_msg.right = volt[1];
+            volt_pub_->publish(volt_msg);
+            std::cout << "Pub Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
+          }
+          // canbus_slave::msg::FocAngle volt_msg;
+          // volt_msg.left = volt[0];
+          // volt_msg.right = volt[1];
+          // std::cout << "Pub Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
+          // volt_pub_->publish(volt_msg);
 
           // update the previous value
           joy_prev[number] = joy[number];
+          volt_prev[number] = volt[number];
         }
         else return;
       }
@@ -92,20 +143,20 @@ private:
         return;
       }
 
-      // convert to voltage
-      float volt[2] = {0.0f, 0.0f};
-      joy2theworld(joy[0], joy[1], volt[0], volt[1]);
+      // // convert to voltage
+      // float volt[2] = {0.0f, 0.0f};
+      // joy2theworld(joy[0], joy[1], volt[0], volt[1]);
 
-      // publish the message
-      canbus_slave::msg::FocAngle volt_msg;
-      volt_msg.left = volt[0];
-      volt_msg.right = volt[1];
-      volt_pub_->publish(volt_msg);
-      std::cout << "Pub Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
+      // // publish the message
+      // canbus_slave::msg::FocAngle volt_msg;
+      // volt_msg.left = volt[0];
+      // volt_msg.right = volt[1];
+      // volt_pub_->publish(volt_msg);
+      // std::cout << "Pub Voltage Left: " << volt[0] << " Right: " << volt[1] << std::endl;
 
-      // update the previous value
-      joy_prev[number] = joy[number];
-      return;
+      // // update the previous value
+      // joy_prev[number] = joy[number];
+      // return;
     }
   }
 };
